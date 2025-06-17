a1a4f73f53184b97e160c8220d799ea64e431683
Arvin Wu <arvin.wu@sap.com>
2025-06-17 16:04:37 +0800

    Fix default router definition.

--- Mission - Develop a Node.js Application in the SAP BTP Kyma Runtime/kyma-multitenant-node/routes/index.js
+++ Mission - Develop a Node.js Application in the SAP BTP Kyma Runtime/kyma-multitenant-node/routes/index.js
@@ -2,24 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 /**
- * This is the default end-point when someone attempts to access the SaaS application.
- * We show a message to the logged in user.
- * Format of the message: Hello <logon name>; your tenant subdomain is <consumer sub-domain>; your tenant zone id is <consumer tenant id>
- * The logon name will be specific to each user.
- * The tenant zone and sub domain will be the same for all users of one consumer(tenant).
- * Otherwise, if there is no AuthInfo object found, We show the message "Hello World" to users.
+ * This is the default end-point when someone accesses the SaaS application.
+ * Always returns: "Hello World!"
+ * If any error occurs, responds with status 500.
  */
 router.get("/", function(req, res, next) {
     try {
-        var line1 = "Hello " + req.authInfo.getLogonName();
-        var line2 = "your tenant sub-domain is " + req.authInfo.getSubdomain();
-        var line3 = "your tenant zone id is " + req.authInfo.getZoneId();
-        var responseMsg = line1 + "; " + line2 + "; " + line3;
-        res.send(responseMsg);
+        // Return 'Hello World!' since auth info is not available in this app
+        res.send("Hello World!");
     } catch (e) {
-        console.log("AuthInfo object undefined.");
-        var responseMsg = "Hello World!";
-        res.send(responseMsg);
+        res.status(500).send("Server error occurred: " + e.message);
     }
 });
 
